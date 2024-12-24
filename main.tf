39cbd9c08a7c6b0489ea701e5bc72ea82dc2105d
Dinesh1496 <dinesh1496.kumar@gmail.com>
2024-12-24 12:25:56 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,14 +1,35 @@
 provider "aws" {
-    region = "us-east-1"
-    access_key = "AKIASFIXDEFPZOKGNLCS"
-    secret_key = "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"
+    region = "us-west-1"
+    profile = "default"
+}
+  
+provider "vault" {
+    address = "http://13.52.247.233:8200"
+    skip_child_token =  true
+
+    auth_login {
+        path = "auth/approle/login"
+        
+        parameters = {
+            role_id = "c2005b4c-1ef8-2474-de59-c7bc04085a9b"
+            secret_id = "99e9b3ed-737d-7238-dc5a-811c9c1dd81c"
+            
+        }
+    }
 }
 
-module "ec2_instance"  {
-    source = "./modules_ec2-instance"
-
-    ami_value= "ami-01af4173f6cee270a"
-    instance_type_value =  "t2.micro"
-    subnet_id_value = "subnet-074304bca19efa59f"
-    key_name_value = "cloudwatch-key"
+data "vault_kv_secret_v2" "example" {
+  mount = "kv"
+  name  = "test-secret"
 }
+
+resource "aws_instance" "example" {
+    ami = "ami-0da424eb883458071"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "test-instance"
+        secret = data.vault_kv_secret_v2.example.data["aws"]
+    }
+  
+}
+
